81cc2e57ea927d772f29e35e8bd41debf0e2426e
Airan Vega <airan_234@hotmail.com>
2019-09-12 20:08:52 +0100

    model finished

---
+++ Back/api/v1/events/events.model.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+
+const EVENTschema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "The title is required"],
+        max: 100
+    },
+
+    date: {
+        type: String,
+        required: [true, "The date is required"],
+        validate: /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/
+    },
+
+    hour: {
+        type: String,
+        required: [true, "The hour is required"],
+        validate: /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/
+    },
+
+    place: {
+        type: String,
+        required: [true, "The place is required"],
+        max: 100 
+    },
+
+    image: {
+        type: String
+    },
+
+    gender: {
+        type: Number,
+        required: [true, "The gender is required"],
+        enum: [0, 1, 2]
+    },
+
+    category: {
+        type: Array,
+        required: [true, "The category is required"],
+        validate: [arrayLength, "Unapropiate size"],
+    },
+
+    description: {
+        type: String,
+        required: [true, "The description is required"],
+        max: 800
+    },
+
+    capacity: {
+        type: Number
+    },
+
+    sailingClub: {
+        type: String,
+        required: [true, "The description is required"],
+        max:100
+    },
+
+    CreatedAt: {
+        type: Date,
+        default: Date.now()
+    }
+
+});
+
+// Returns true if the length of the array is 2
+function arrayLength(length){
+    return length == 2;
+}
+
+const EVENT = mongoose.model("event", EVENTschema);
+module.exports = EVENT;
