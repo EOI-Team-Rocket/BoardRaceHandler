aa8d33f857d03e2006b8fc4f99a213618c930a80
Airan Vega <airan_234@hotmail.com>
2019-09-13 17:24:15 +0100

    modified the url of the put in the routes.js file

--- Back/api/v1/routes.js
+++ Back/api/v1/routes.js
@@ -3,7 +3,7 @@ const controller = require("./events/events.controller");
 
 router.get("/events", controller.readAllEvents);
 router.get("/event/:id", controller.readOneEvent);
-router.put("updateEvent/:id", controller.updateEvent);
+router.put("/updateEvent/:id", controller.updateEvent);
 router.delete("/event/:id", controller.deleteEvent);
 
 module.exports = router;
