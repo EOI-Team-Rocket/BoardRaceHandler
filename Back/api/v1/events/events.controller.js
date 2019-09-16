c8522d003940cc29fe37f894c3f919a806ee7b54
Airan-Vega <53567068+Airan-Vega@users.noreply.github.com>
2019-09-16 20:00:21 +0100

    Update Back/api/v1/events/events.controller.js
    Co-Authored-By: Echedey Garcias <eche2312@gmail.com>

--- Back/api/v1/events/events.controller.js
+++ Back/api/v1/events/events.controller.js
@@ -10,7 +10,7 @@ module.exports = {
 
 function createEvent(req, res){
     return EVENTModel.create(req.body)
-        .then(responde => {
+        .then(response => {
             res.status(200).json(response)
         })
         .catch((err) => handdleError(err, res));
