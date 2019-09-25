b80e80971363d9c541b0f83b8226b4d568803f32
Echedey Garcias <eche2312@gmail.com>
2019-09-25 20:55:33 +0100

    change model events

--- Back/api/v1/events/events.controller.js
+++ Back/api/v1/events/events.controller.js
@@ -10,7 +10,6 @@ module.exports = {
 }
 
 function createEvent(req, res) {
-    console.log(req.body)
     return EVENTModel.create(req.body)
         .then(response => {
             res.status(200).json(response);
