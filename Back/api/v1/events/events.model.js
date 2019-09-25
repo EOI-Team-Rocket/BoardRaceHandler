000a3c1cfc568d8f2b127af6d6342d77237466c7
Echedey Garcias <eche2312@gmail.com>
2019-09-25 19:17:15 +0100

    review

--- Back/api/v1/events/events.model.js
+++ Back/api/v1/events/events.model.js
@@ -5,78 +5,76 @@ const EVENTschema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, "The title is required"],
-        // max: 100
     },
 
     date: {
         type: String,
         required: [true, "The date is required"],
         //dd/mm/yyyy
-        validate: /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/
+        validate: /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/ 
     },
 
     hour: {
         type: String,
         required: [true, "The hour is required"],
         //hh:mm
-        validate: /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/
+        validate: /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/ 
     },
 
     place: {
         type: String,
         required: [true, "The place is required"],
-        // max: 100
     },
 
     image: {
-        type: String
+        type: String //search
     },
 
     gender: {
         type: String,
         required: [true, "The gender is required"],
-        // enum: ["H", "M", "X"]
+        enum: ["H", "M", "X"]
     },
 
-    boat_category: {
+    class: {
         type: String,
+        enum: ["420", "470", "29-ER", "49-ER", "Crucero", "Hansa 303", "Ideal 18", "J-80", "Kiteboarding",
+            "Laser 4.7", "Laser Radial", "Nacra-17", "Optimist", "Radio Control", "Sin Clase", "Snipe",
+            "Thecno", "Vela Adaptada Iniciacion", "Windsurf/Fun Board", "Windsurf/RSX", "Windsurf/Velocidad"],
+        required: [true, "The class is required"]
+    },
+
+    category: {
+        type: String,
+        enum: ["Infantil", "Iniciacion Infantil", "Juvenil", "Senior", "Ampliacion", "Ampliacion de Autonomica"
+            , "Autonomica"],
         required: [true, "The category is required"]
     },
 
     description: {
         type: String,
-        required: [true, "The description is required"],
-        // max: 800
-    },
-
-    age_category: {
-        type: String,
-/*         enum: ["Infantil", "Iniciacion Infantil", "Juvenil", "Senior", "Ampliacion", "Ampliacion de Autonomica"
-            , "Autonomica"], */
-        required: true
+        required: [true, "The description is required"]
     },
 
     capacity: {
-        type: String
+        type: Number
+        // add validate > 0
     },
 
-    sailingClub: {
+    manager: {
         type: String,
-        // required: [true, "The description is required"],
+        required: [true, "The manager is required"]
     },
 
     createdAt: {
         type: Date,
-        default: Date.now()
+        default: Date.now() //create function because will take the server starts date
     },
 
-    competition: {
-        type: String
-    },
 
     participants: [{
-        type: String,
-        ref: 'User'
+        type: Schema.Types.ObjectId,
+        ref: 'user'
     }]
 
 
