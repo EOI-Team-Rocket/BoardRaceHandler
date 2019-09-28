168faae748e1d98d028064752bc50906500d9767
Allwka <38868947+AllWKA@users.noreply.github.com>
2019-09-28 12:50:11 +0100

    Update front/src/components/eventItem.vue
    Co-Authored-By: 6daniel32 <6daniel32@gmail.com>

--- front/src/components/eventItem.vue
+++ front/src/components/eventItem.vue
@@ -46,7 +46,7 @@ export default {
     };
   },
   methods: {
-    showModalDelete() {
+    toggleModalDelete() {
       this.deleteModalIsShown = !this.deleteModalIsShown;
     },
     refreshList() {
