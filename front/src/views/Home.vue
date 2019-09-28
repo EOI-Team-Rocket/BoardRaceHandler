31f1fc484996514f0ae19f10807e1c6e55a7926d
Allwka <bryanjaramillobaldeon@gmail.com>
2019-09-28 13:46:39 +0100

    carousel Done

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="home">
     <HelloWorld />
+    <div class="carouselContainer">
+      <Carousel resource="events" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Carousel from "@/components/Carousel";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Carousel
   }
 };
 </script>
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+/* center carosuel */
+.carouselContainer {
+  margin-left: 5%;
+  width: 90%;
+  height: 90%;
+}
+.helloWorldContainer {
+}
+</style>
