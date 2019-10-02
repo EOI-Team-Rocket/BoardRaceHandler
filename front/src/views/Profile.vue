25bd343cb00d74b2f93f139a32f4f2370bffc789
Airan-Vega <53567068+Airan-Vega@users.noreply.github.com>
2019-10-03 00:27:11 +0100

    Update front/src/views/Profile.vue
    Co-Authored-By: 6daniel32 <6daniel32@gmail.com>

--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -90,7 +90,7 @@ export default {
             this.personalInfo = res.data.personalInfo;
             this.sportInfo = res.data.sportInfo;
 
-            console.log("kdjekjkej");
+            
           })
           .catch(err => {
             this.error = err;
